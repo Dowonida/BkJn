0f2eb0b483dac396af398e88c9edb63416aff6ec
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-08-15 13:35:03 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 520 ms, Memory: 241048 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,12 @@
+A=input()
+B=input()
+
+DP=[[0]*(len(A)+1) for i in range(len(B)+1)]
+rst=0
+for i in range(len(B)):
+    for j in range(len(A)):
+        if B[i]==A[j]:
+            DP[i+1][j+1]=DP[i][j]+1
+            rst=max(DP[i+1][j+1],rst)
+
+print(rst)
