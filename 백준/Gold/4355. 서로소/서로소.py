6a41a103d853ca407c3a097bd81a4982acd91514
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-08-16 20:55:09 +0900

    [Gold I] Title: 서로소, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/4355. 서로소/서로소.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+def func(n):
+    m=n
+    for i in range(2,int(n**(0.5)+1)):
+        if m%i==0:
+            while m%i==0:
+                m//=i
+            n=(n//i)*(i-1)
+    if m>1:
+        n=(n//m)*(m-1)
+    return n
+
+while True:
+    a=int(input())
+    if a==0:
+        break
+    print(func(a))
