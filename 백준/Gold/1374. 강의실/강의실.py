1d2045c443e1a3a8a8f4da6dcf33318115e91b38
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-08-28 15:11:24 +0900

    [Gold V] Title: 강의실, Time: 400 ms, Memory: 45980 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -3,22 +3,21 @@ import sys
 input=sys.stdin.readline
 
 N= int(input())
-
-C = [0]*N
 M = []
+
 for i in range(N):
     a,b,c=map(int,input().split())
-    M.append((b,c))
-M.sort(key=lambda x:x[0])#시작시간 정렬
+    heappush(M,(b,c))
+
+
 stack=[0]
 
 while M:
-    i=heappop(M)
-    a=heappop(stack)
-    if a<=i[0]:
-        heappush(stack,i[1])
+    if stack[0]<=M[0][0]:
+        heappop(stack)
+        heappush(stack,heappop(M)[1])
     else:
-        heappush(stack,i[1])
-        heappush(stack,a)
-
+        heappush(stack,heappop(M)[1])
+        
+        
 print(len(stack))
