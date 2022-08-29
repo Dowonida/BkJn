aa750d033d76f563ce4e9973a3909e932ec796d8
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-08-29 23:38:18 +0900

    [Gold II] Title: 공항, Time: 264 ms, Memory: 92148 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -8,16 +8,24 @@ def get(a):
     else:
         dic[a]=get(dic[a])
         return dic[a]
-N=int(input())
-dic={i : i for i in range(N+1)}
 
-M=int(input())
-for i in range(M):
+def union(a):
+    x=get(a)
+    dic[x]-=1
+
+
+N=int(input())
+K=int(input())
+#L= [ int(input()) for i in range(N)]
+
+dic={i:i for i in range(N+1)}
+
+for i in range(1,1+K):
     a=int(input())
-    if dic[get(a)]:
-        dic[get(a)]=get(a)-1
-    else:
-        print(i)
+    if get(a)==0:
+        print(i-1)
         break
+    union(a)
 else:
-    print(N)
+    print(K)
+    #print(i,dic)
