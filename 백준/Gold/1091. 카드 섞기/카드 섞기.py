68c4753406cbae1227817bc2a711fbc23d880363
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-09-13 10:38:10 +0900

    [Gold IV] Title: 카드 섞기, Time: 140 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1091. 카드 섞기/카드 섞기.py
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.py
@@ -19,14 +19,15 @@ for i in range(N):
         while j!=i:
             cnt.append(j)
             j=M[j]
+        cnt2=list(map(lambda x:x%3, cnt))
         for j in range(len(cnt)):
-            dic[cnt[j]]=cnt[j:]+cnt[:j]
+            dic[cnt[j]]=cnt2[j:]+cnt2[:j]
             L[cnt[j]]=len(cnt)
-            Max=lcm(Max,len(cnt))
+        Max=lcm(Max,len(cnt))
         
 for i in range(Max):
     for j in dic:
-        if want[j]!=dic[j][i%L[j]]%3:
+        if want[j]!=dic[j][i%L[j]]:
             break
     else:
         print(i)
