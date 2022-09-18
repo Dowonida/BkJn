5348d02f90047ceeefd3a128bd0fc6f749bb1685
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-09-18 17:01:16 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -9,7 +9,7 @@ while True:
         a=int(input())
         
         while not(STK[-1][0]<a<STK[-1][2]):
-            RST.append(STK.pop()[1])
+            print(STK.pop()[1])
         if a>STK[-1][1]:
             STK.append((STK[-1][1],a,STK[-1][2]))
         else:
@@ -19,6 +19,4 @@ while True:
     except:
         break
 while len(STK)>1:
-    RST.append(STK.pop()[1])
-for i in RST:
-    print(i)
+    print(STK.pop()[1])
