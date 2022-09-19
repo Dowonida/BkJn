25a99446c613b7499666589e8b9491dfae42d792
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-09-19 22:14:23 +0900

    [level 3] Title: 등굣길, Time: 2.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,13 @@
+def solution(m, n, puddles):
+    answer = 0
+    #가로먼저-> 세로 
+    #
+    DP=[[0]*(n+1) for i in range(m+1)]
+    DP[1][1]=1
+    for i in range(1,m+1): #i가 가로 [가로][세로]
+        for j in range(1,n+1):
+            if [i,j] in puddles:
+                continue
+            DP[i][j]+=DP[i-1][j]+DP[i][j-1]
+    
+    return DP[-1][-1]%1000000007
