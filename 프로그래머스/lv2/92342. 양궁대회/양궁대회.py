d1c31ff22f66f6fb822b617fcc42e27c5b053147
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-09-21 21:10:09 +0900

    [level 2] Title: 양궁대회, Time: 11.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,14 +1,19 @@
 def solution(n, info):
     Ap= -sum([(10-i) for i in range(11) if info[i]>0])
     Bp= Ap
+    #print(Bp)
     need=[]
     for i in info:
         need.append(i+1)
+        
     answers = []
+    que=[]
+    # (idx,score,visited)
     for i in range(2048):
         visited=[ (bool((2**j)&i))*need[j] for j in range(11)]
         if sum(visited)>n:
             continue
+        
         score=0
         for i in range(11):
             if visited[i]:
@@ -24,6 +29,7 @@ def solution(n, info):
             rest=n-sum(visited)
             visited[-1]+=rest
             answers.append(visited)
+        
     if Bp<=0:
         return [-1]
     return answers[-1]
