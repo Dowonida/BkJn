aa3c99e64f58efe6b523a70706cfe0056e46aa53
Dowonida <100022949+Dowonida@users.noreply.github.com>
2022-09-21 19:06:44 +0900

    [Gold III] Title: 교환, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1039. 교환/교환.py
+++ 백준/Gold/1039. 교환/교환.py
@@ -1,31 +1,34 @@
 N , K = input().split()
 K=int(K)
 M=len(N)
-if ((len(N)==2 and N[1]=='0') or len(N)==1) and K:
+if ((len(N)==2 and N[1]=='0') or len(N)==1) and K:#교환못하면 ㅂㅇㅂㅇ
     print(-1)
 else:
     Max=''
     que={(N,K)}
     RST=[]
+    visited={(N,K%2)}
     while que:
         nq=set()
-        for n,k in que:
+        for n,k in que: #커지게 
             flag=False
             if k>0:
                 for i in range(M-1):
                     for j in range(i+1,M):
                         if n[i]<n[j]:
                             nn=n[:i]+n[j]+n[i+1:j]+n[i]+n[j+1:]
-                            nq.add((nn,k-1))
-                            flag=True
-            if not flag:
-                if k%2:
-                    for i in range(10):
+                            if (nn,(k-1)%2) not in visited:
+                                visited.add((nn,(k-1)%2))
+                                nq.add((nn,k-1))
+                                flag=True
+            if not flag:#더 바꿀게 없으면 후보
+                if k%2:#남은 교환횟수 털기 
+                    for i in range(10):#같은거 2개 있으면 둘이 교환
                         if n.count(str(i))>1:
                             break
-                    else:
+                    else:#아니면 가장 적게 손해보기=끝에서 2개 교환
                         n=n[:-2]+n[-1]+n[-2]
-                Max=max(Max,''.join(n))
+                Max=max(Max,''.join(n))#교환 다 소진했으면 Max 새로고침
         que=nq
     print(Max)
 
