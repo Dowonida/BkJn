4aa975f2ceb262b555b8fe77613236c238b8a25b
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-02-26 12:52:13 +0900

    [Platinum III] Title: 성대나라의 물탱크, Time: 680 ms, Memory: 160860 KB -BaekjoonHub

--- 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.py
+++ 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.py
@@ -1,56 +1,51 @@
 import sys
-sys.setrecursionlimit(300000)
 input = sys.stdin.readline
 
 N, C = map(int,input().split())
 
 adj = [ [] for _ in range(N+1)]
-
 for _ in range(N-1):
     a,b = map(int,input().split())
     adj[a].append(b)
     adj[b].append(a)
 
-
 depth = [0]*(N+1)
-cnt = N #방문순서이자 세그먼트 인덱스 
+S_idx = [0]*(N+1)
+E_idx = [0]*(N+1)
 depth[C] = 1
-parent = [0]*(N+1)
-seg_idx = [0]*(N+1) #세그먼트에서 몇번째인지
-seg_idx[C] = N
-last_idx = [0]*(N+1) #내 서브트리에서 마지막 방문 번호
-last_idx[C] = N
-n_idx =[0]*(N+1) #마지막 처리 번호 
-stk = [C]
+cnt = N
+S_idx[C] = N
+E_idx[C] = N+1
 
+stk = [C]
 while stk:
     cur = stk[-1]
-    if n_idx[cur]<len(adj[cur]):
-        nxt = adj[cur][n_idx[cur]]
-        n_idx[cur] += 1
+    if adj[cur]:
+        nxt = adj[cur].pop()
         if depth[nxt]:
             continue
-        stk.append(nxt)
-        parent[nxt] = cur
-        depth[nxt] = depth[cur]+1
         cnt += 1
-        seg_idx[nxt] = cnt
-        last_idx[nxt] = cnt
+        S_idx[nxt] = cnt
+        E_idx[nxt] = cnt+1
+        depth[nxt] = depth[cur]+1
+        stk.append(nxt)
     else:
-        last_idx[parent[cur]] = last_idx[cur]
         stk.pop()
-    
+        if stk:
+            E_idx[stk[-1]] = E_idx[cur]
+        
+
 seg = [0]*2*N
 for _ in range(int(input())):
     a,b = map(int,input().split())
-    if a== 1:
-        idx = seg_idx[b]
+    if a==1:
+        idx=S_idx[b]
         while idx:
-            seg[idx] += 1
-            idx //= 2
+            seg[idx]+=1
+            idx//=2
     else:
-        s,e = seg_idx[b], last_idx[b]+1
-        rst = 0
+        rst =0
+        s,e = S_idx[b], E_idx[b]
         while s<e:
             if s%2:
                 rst += seg[s]
