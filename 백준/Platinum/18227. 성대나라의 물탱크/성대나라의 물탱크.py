33a64acc657812a66564b81a8737d3e12c3f4328
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-02-26 12:50:30 +0900

    [Platinum III] Title: 성대나라의 물탱크, Time: 1892 ms, Memory: 87824 KB -BaekjoonHub

--- 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.py
+++ 백준/Platinum/18227. 성대나라의 물탱크/성대나라의 물탱크.py
@@ -13,67 +13,51 @@ for _ in range(N-1):
 
 
 depth = [0]*(N+1)
+cnt = N #방문순서이자 세그먼트 인덱스 
 depth[C] = 1
-#sons = [ [] for _ in range(N+1)] #자식노드 번호
-sub_count = [1]*(N+1) #서브트리 크기 
-chain_info = [0]*(N+1) #몇번 체인에 속하는지 (a,b꼴로 저장) a번체인 b번순서
-chains = []
 parent = [0]*(N+1)
-#자식이 전부 처리되었으면 체인번호를 정한다.
-#자식이 없다면 새 체인번호 부여,
-#자식이 있다면 가장 큰 서브트리를 가지는 자식과 동일하게 부여
+seg_idx = [0]*(N+1) #세그먼트에서 몇번째인지
+seg_idx[C] = N
+last_idx = [0]*(N+1) #내 서브트리에서 마지막 방문 번호
+last_idx[C] = N
+n_idx =[0]*(N+1) #마지막 처리 번호 
+stk = [C]
 
-def dfs(node=C):
-    sons_cnt = 0
-
-    for i in adj[node]:
-        if depth[i]:
+while stk:
+    cur = stk[-1]
+    if n_idx[cur]<len(adj[cur]):
+        nxt = adj[cur][n_idx[cur]]
+        n_idx[cur] += 1
+        if depth[nxt]:
             continue
-        depth[i] = depth[node]+1
-        parent[i] = node
-        dfs(i)
-        sub_count[node] += sub_count[i]
-        if sub_count[i]>sons_cnt:
-            sons_cnt = sub_count[i]
-            chain_info[node] = (chain_info[i][0],chain_info[i][1]+1)
-
-    if sons_cnt == 0:
-        chain_info[node] = (len(chains),0)
-        chains.append([])
-    chains[chain_info[node][0]] += [0,0]
-    chains[chain_info[node][0]][0] = parent[node], chain_info[node][1]+1
-    #어차피 0번은 결번이니까 올라갈 노드번호, 체인길이를 저장 
-
-dfs()
-        
+        stk.append(nxt)
+        parent[nxt] = cur
+        depth[nxt] = depth[cur]+1
+        cnt += 1
+        seg_idx[nxt] = cnt
+        last_idx[nxt] = cnt
+    else:
+        last_idx[parent[cur]] = last_idx[cur]
+        stk.pop()
+    
+seg = [0]*2*N
 for _ in range(int(input())):
     a,b = map(int,input().split())
-    if a==1:
-        while b:
-            chain, s = chain_info[b]
-            seg=chains[chain]
-            s += seg[0][1]
-            e = seg[0][1]*2
-            b = seg[0][0]
-
-            while s<e:
-                if s%2:
-                    seg[s] += 1
-                    s += 1
-                if e%2:
-                    e -= 1
-                    seg[e] += 1
-                s//=2
-                e//=2
-        continue
+    if a== 1:
+        idx = seg_idx[b]
+        while idx:
+            seg[idx] += 1
+            idx //= 2
     else:
-        chain, s = chain_info[b]
-        seg=chains[chain]
-        s += seg[0][1]
+        s,e = seg_idx[b], last_idx[b]+1
         rst = 0
-        scale = depth[b]
-        while s:
-            rst += seg[s]*scale
+        while s<e:
+            if s%2:
+                rst += seg[s]
+                s += 1
+            if e%2:
+                e -= 1
+                rst += seg[e]
             s//=2
-        print(rst)
-
+            e//=2
+        print(rst*depth[b])
