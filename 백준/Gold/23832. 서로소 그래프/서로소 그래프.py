16a3674826a62991422a204338822d6e99ea2ddd
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-06-04 00:40:07 +0900

    [Gold I] Title: 서로소 그래프, Time: 140 ms, Memory: 31644 KB -BaekjoonHub

--- 백준/Gold/23832. 서로소 그래프/서로소 그래프.py
+++ 백준/Gold/23832. 서로소 그래프/서로소 그래프.py
@@ -1,20 +1,28 @@
-import sys
-input=sys.stdin.readline
+N = int(input())
+L = [0]*(N+1)
+prime = []
 
-def func(n):
-    m=n
-    for i in range(2,int(n**(0.5)+1)):
-        if m%i==0:
-            while m%i==0:
-                m//=i
-            n=(n//i)*(i-1)
-    if m>1:
-        n=(n//m)*(m-1)
-    return n
-
-
-N=int(input())
-rst=0
 for i in range(2,N+1):
-    rst+=func(i)
+    if L[i] == 0:
+        L[i] = i
+        prime.append(i)
+    for p in prime:
+        n = i*p
+        if i*p>N:
+            break
+        L[n] = p
+        if i%p == 0:
+            break
+rst = 0
+for i in range(2,N+1):
+    ii = i
+
+    while i>1:
+        cnt = L[i]
+        ii = ii*(L[i]-1)//L[i]
+        while i%cnt == 0 and i>1:
+            i//=cnt
+    rst += ii
+
 print(rst)
+    
