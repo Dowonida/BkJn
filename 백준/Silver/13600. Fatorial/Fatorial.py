0f0f7836cfaa1c378b478204eae80279d7993cd0
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-11-27 20:28:16 +0900

    [Silver I] Title: Fatorial, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13600. Fatorial/Fatorial.py
+++ 백준/Silver/13600. Fatorial/Fatorial.py
@@ -1,12 +1,10 @@
 N = int(input())
 
-L = [0]*(100001)
-F = []
-cnt = 1
+F = [1]
+
 for i in range(2,10):
-    F.append(cnt)
-    L[cnt] = 1
-    cnt *= i
+    F.append(i*F[-1])
+    
 rst = 0
 for i in F[::-1]:
     rst += N//i
