be736b070babbb607777382162ad2d874a45f440
Dowonida <100022949+Dowonida@users.noreply.github.com>
2023-12-30 18:21:36 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 140.97 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,24 +1,22 @@
+import heapq
+
 def solution(food_times, k):
-    answer = 0
-    L=len(food_times)
-    
-    yang={i:[] for i in sorted(food_times)}
-    for i in range(L):
-        yang[food_times[i]].append(i)
-    Y=sorted(list(yang.keys())) # Y=[1,2,3]
-    food_times=[-100]+food_times
-    if Y[0]*L>=k:
-        k%=L
-        return k+1
-    cnt=L-len(yang[Y[0]])#남은 음식 수
-    time=Y[0]*L
-    for i in range(1,len(Y)):
-        time+= cnt*(Y[i]-Y[i-1])
-        if time>k:
-            time-=cnt*(Y[i]-Y[i-1])
-            break
-        cnt-=len(yang[Y[i]])
-    else:
-        return -1
-    answer=[j for j in range(1,1+L) if food_times[j]>=Y[i] ][(k-time)%cnt]
-    return answer
+    if sum(food_times) <= k:
+        return - 1
+
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i + 1))
+
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1
+        previous = now
+
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k - sum_value) % length][1]
